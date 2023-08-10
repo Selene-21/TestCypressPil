1deaad1b0faebaea8afc4f4ed70d0b8700f6dccb
Selene-21 <jadelu2919@gmail.com>
2023-08-10 10:07:51 -0300

    correccion test

--- cypress/e2e/EDEN/eden.cy.js
+++ cypress/e2e/EDEN/eden.cy.js
@@ -2,28 +2,26 @@
 import Edenpage from "../../PageObject/Edenpage"
 const edenpage = new Edenpage();
 
-describe("test sobre la pagina EDEN ENTRADAS", () => {
-    it('verificar subtitulos', () => {
-      cy.visit('https://www.edenentradas.com.ar/sitio/contenido/inicio');
-      Edenpage.getsubtitles().first().should('contain.text', 'BUSCAR EVENTO')
-      Edenpage.getsubtitles().last().should('contain.text', 'CALENDARIO DE EVENTOS')
-  
-      })
-    })
-    it('verificar MENU', () => {
-      cy.visit('https://www.edenentradas.com.ar/sitio/contenido/inicio')
-      Edenpage.getmenuButtons().eq(0).should('contain.text', 'HOME')
-      Edenpage.getmenuButtons().eq(1).should('contain.text', 'TODOS')
-      Edenpage.getmenuButtons().eq(2).should('contain.text', 'AGENDA DEL FINDE')
-      Edenpage.getmenuButtons().eq(3).should('contain.text', 'RECITALES')
-      Edenpage.getmenuButtons().eq(4).should('contain.text', 'TEATROS')
-      Edenpage.getmenuButtons().eq(5).should('contain.text', 'CUARTETOS')
-      Edenpage.getmenuButtons().eq(6).should('contain.text', 'FESTIVALES')
-      Edenpage.getmenuButtons().eq(7).should('contain.text', 'SALAS')
-      })
+describe('test sobre la página EDEN ENTRADAS', () => {
+  it('verificar subtitulos', () => { 
+    cy.visit('https://www.edenentradas.com.ar/sitio/contenido/inicio');
+    edenpage.getsubtitles().first().should('contain text', 'BUSCAR EVENTO')
+    edenpage.getsubtitles().last().should('contain text', 'CALENDARIO DE EVENTOS')
+  })
 
-      it('Verificar página RECITALES', () => {
-        cy.visit('https://www.edenentradas.com.ar/');
+it('verificar MENU', () =>{ 
+  cy.visit('https://www.edenentradas.com.ar/sitio/contenido/inicio');
+  edenpage.getmenuButtons().eq(0).should('contain text', 'HOME')
+  edenpage.getmenuButtons().eq(1).should('contain.text', 'TODOS')
+  edenpage.getmenuButtons().eq(2).should('contain.text', 'AGENDA DEL FINDE')
+  edenpage.getmenuButtons().eq(3).should('contain.text', 'RECITALES')
+  edenpage.getmenuButtons().eq(4).should('contain.text', 'TEATROS')
+  edenpage.getmenuButtons().eq(5).should('contain.text', 'CUARTETOS')
+  edenpage.getmenuButtons().eq(6).should('contain.text', 'FESTIVALES')
+  edenpage.getmenuButtons().eq(7).should('contain.text', 'SALAS')
+})
 
-        Edenpage.getmenuButtons().eq(3).click();
-      } )
+it('verificar RECITALES', () => {
+  cy.visit('https://www.edenentradas.com.ar/');
+  edenpage.getmenuButtons().eq(3).click();
+})})
